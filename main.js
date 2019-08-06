72a58e928c85fe9cc5d0e288ac23029b8c887aa6
DarksomeV <viktor@MacBook-Pro-Viktor.local>
2019-08-06 23:22:54 +0300

    initial commit. Changeble interface

---
+++ main.js
@@ -0,0 +1,38 @@
+import Enumerable from './Enumerable.js';
+
+const myVar = new Enumerable();
+
+const cars = [
+    { brand: 'bmw', model: 'm5', year: 2014 },
+    { brand: 'bmw', model: 'm4', year: 2013 },
+    { brand: 'kia', model: 'sorento', year: 2014 },
+    { brand: 'kia', model: 'rio', year: 2010 },
+    { brand: 'kia', model: 'sportage', year: 2012 },
+];
+
+const coll = new Enumerable(cars);
+
+const result = coll.orderBy(car => car.model, 'asc').toArray();
+
+console.log(result);
+
+var items = [
+    { name: 'Edward', value: 21 },
+    { name: 'Sharpe', value: 37 },
+    { name: 'And', value: 45 },
+    { name: 'The', value: -12 },
+    { name: 'Magnetic' },
+    { name: 'Zeros', value: 37 }
+];
+items.sort(function (a, b) {
+    if (a.name > b.name) {
+        return 1;
+    }
+    if (a.name < b.name) {
+        return -1;
+    }
+    // a должно быть равным b
+    return 0;
+});
+
+// console.log(items);
